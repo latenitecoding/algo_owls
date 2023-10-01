53851048712ed30f895ddca7ff3e7e061dfb0772
latenitecoding <latenitecode@gmail.com>
2023-10-01 13:28:42 -0500

    updates name to solution in Solutions

--- src/solutions/Solutions.java
+++ src/solutions/Solutions.java
@@ -1,9 +1,9 @@
 public class Solutions {
 
-  public static void callSolution(String name) {
-    switch (name) {
+  public static void callSolution(String solution) {
+    switch (solution) {
       default:
-        System.out.printf("No solution for %s\n", name);
+        System.out.printf("No solution for %s\n", solution);
     }
   }
 }
