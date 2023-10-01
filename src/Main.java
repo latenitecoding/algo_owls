7b7cc3b4ce1408c4cf5084737ff0f160debe7e7b
latenitecoding <latenitecode@gmail.com>
2023-10-01 08:59:15 -0500

    adds entry point for Java CLI and solutions index.

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+  public static void main(String[] args) {
+    if (args.length > 0 && (args[0].equals("-h") || args[0].equals("--help"))) {
+      System.out.println("Usage: algo_owls --solution <SOLUTION>");
+      System.out.println();
+      System.out.println("Options:");
+      System.out.printf("%-30s %s\n", "  -s, --solution <SOLUTION>", "");
+      System.out.printf("%-30s %s\n", "  -h, --help", "Print help");
+      System.out.printf("%-30s %s\n", "  -V, --version", "Print version");
+      System.exit(0);
+    }
+    if (args.length > 0 && (args[0].equals("-V") || args[0].equals("--version"))) {
+      System.out.println("algo_owls 0.1.0");
+      System.exit(0);
+    }
+    if (args.length > 1 && (args[0].equals("-s") || args[0].equals("--solution"))) {
+      Solutions.callSolution(args[1]);
+      System.exit(0);
+    }
+    System.out.println("Usage: algo_owls --solution <SOLUTION>");
+    System.out.println();
+    System.out.println("For more information, try '--help'.");
+    System.exit(1);
+  }
+}
---
+++ src/solutions/Solutions.java
@@ -0,0 +1,9 @@
+public class Solutions {
+
+  public static void callSolution(String name) {
+    switch (name) {
+      default:
+        System.out.printf("No solution for %s\n", name);
+    }
+  }
+}
