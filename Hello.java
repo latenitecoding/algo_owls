3425072371ca8540cd0400ac3b28d8bc0f46548a
latenitecoding <latenitecode@gmail.com>
2023-10-19 03:20:58 -0500

    adds Tuple record and print helpers to template

--- Hello.java
+++ Hello.java
@@ -4,12 +4,14 @@ import java.util.stream.*;
 
 public class Hello {
   
-  private static boolean DEBUG = false;
+  private static final boolean DEBUG = false;
   private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
   // ========================================================
-  // StdIn Helpers
+  // Helpers
   // ========================================================
+
+  private static record Tuple(int a, int b) {}
 
   @SuppressWarnings("unused")
   private static int next() throws IOException {
@@ -17,23 +19,52 @@ public class Hello {
   }
 
   @SuppressWarnings("unused")
-  private static int[] nextTuple() throws IOException {
+  private static int[] nextArray() throws IOException {
     return Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
   }
 
   @SuppressWarnings("unused")
-  private static void print(int[] arr) {
+  private static Tuple nextTuple() throws IOException {
+    String[] line = reader.readLine().split(" ");
+    return new Tuple(
+        Integer.parseInt(line[0]),
+        Integer.parseInt(line[1])
+      );
+  }
+
+  @SuppressWarnings("unused")
+  private static void print(String label, int[] arr) {
     if (!DEBUG) return;
     System.out.println("");
+    System.out.println("> " + label);
     System.out.println(Arrays.toString(arr));
     System.out.println("");
   }
 
   @SuppressWarnings("unused")
-  private static void print(int[][] arr) {
+  private static void print(String label, int[][] arr) {
     if (!DEBUG) return;
     System.out.println("");
+    System.out.println("> " + label);
     for (int[] row : arr) System.out.println(Arrays.toString(row));
+    System.out.println("");
+  }
+
+  @SuppressWarnings("unused")
+  private static <E> void print(String label, E e) {
+    if (!DEBUG) return;
+    System.out.println("");
+    System.out.println("> " + label);
+    System.out.println(e);
+    System.out.println("");
+  }
+
+  @SuppressWarnings("unused")
+  private static <E> void print(String label, E[] arr) {
+    if (!DEBUG) return;
+    System.out.println("");
+    System.out.println("> " + label);
+    for (E e : arr) System.out.println(e);
     System.out.println("");
   }
 
