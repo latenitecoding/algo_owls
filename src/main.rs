f4963884ba8c4e07927cef972431289d8c7331d6
latenitecoding <latenitecode@gmail.com>
2024-03-10 12:22:47 -0500

    adds template file

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use std::{fmt::Debug, io, str::FromStr};
+
+//============================================
+// StdIn Helpers
+//============================================
+
+#[allow(unused)]
+#[inline(always)]
+fn next<T: FromStr>(buffer: &mut String) -> T
+where
+    <T as FromStr>::Err: Debug,
+{
+    io::stdin().read_line(buffer).unwrap();
+    buffer.trim().parse::<T>().unwrap()
+}
+
+#[allow(unused)]
+#[inline(always)]
+fn next_tuple<T: FromStr>(buffer: &mut String) -> (T, T)
+where
+    <T as FromStr>::Err: Debug,
+{
+    io::stdin().read_line(buffer).unwrap();
+    let mut iter = buffer.trim().split(' ').map(|s| s.parse::<T>().unwrap());
+    (iter.next().unwrap(), iter.next().unwrap())
+}
+
+#[allow(unused)]
+#[inline(always)]
+fn next_vec<T: FromStr>(buffer: &mut String) -> Vec<T>
+where
+    <T as FromStr>::Err: Debug,
+{
+    io::stdin().read_line(buffer).unwrap();
+    buffer
+        .trim()
+        .split(' ')
+        .map(|s| s.parse::<T>().unwrap())
+        .collect::<Vec<T>>()
+}
+
+//============================================
+// Solution
+//============================================
+
+pub fn main() {
+    println!("Hello, world!");
+}
+
