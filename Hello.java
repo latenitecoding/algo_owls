51e7b17cbf33899eeb5843221511422ecc113bf9
latenitecoding <latenitecode@gmail.com>
2025-10-01 05:49:29 -0500

    updates template

--- Hello.java
+++ Hello.java
@@ -3,75 +3,56 @@ import java.util.*;
 import java.util.stream.*;
 
 public class Hello {
-  
-  // ========================================================
-  // Solution
-  // ========================================================
 
   public static void main(String[] args) throws IOException {
-    String line = reader.readLine();
-    System.out.println(line);
+    int n = nextInt();
+    System.out.println(n);
+  }
+   
+  //==================================================================
+  // HELPERS
+  //==================================================================
+
+  static Scanner stdin = new Scanner(System.in);
+
+  static double lastFloat() throws IOException {
+    double d = nextFloat();
+    stdin.nextLine();
+    return d;
   }
 
-  // ========================================================
-  // Helpers
-  // ========================================================
-
-  private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-  private static class Tuple {
-    int n, m;
-
-    public Tuple(int... args) {
-      this.n = args[0];
-      this.m = args[1];
-    }
-
-    // public boolean equals(Object o) {
-    //   if (o instanceof Tuple) {
-    //     Tuple t = (Tuple) o;
-    //     return t.n == n && t.m == m;
-    //   }
-    //   return false;
-    // }
-
-    public String toString() {
-      return String.format("Tuple(%d, %d)", n, m);
-    }
+  static int lastInt() throws IOException {
+    int d = nextInt();
+    stdin.nextLine();
+    return d;
   }
 
-  @SuppressWarnings("unused")
-  private static String next() throws IOException {
-    StringBuilder str = new StringBuilder();
-    int c;
-    while ((c = reader.read()) != -1 && c != ' ' && c != '\n' && c != '\0') {
-      str.append((char) c);
-    }
-    return str.toString();
+  static String next() throws IOException {
+    return stdin.next();
   }
 
-  @SuppressWarnings("unused")
-  private static int[] nextArray() throws IOException {
-    return Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+  static double nextFloat() throws IOException {
+    return stdin.nextDouble();
   }
 
-  @SuppressWarnings("unused")
-  private static float nextFloat() throws IOException {
-    return Float.parseFloat(nextLine());
+  static int nextInt() throws IOException {
+    return stdin.nextInt();
   }
 
-  @SuppressWarnings("unused")
-  private static int nextInt() throws IOException {
-    return Integer.parseInt(nextLine());
+  static String nextLine() throws IOException {
+    return stdin.nextLine();
   }
 
-  @SuppressWarnings("unused")
-  private static String nextLine() throws IOException {
-    return reader.readLine();
+  static int[] nextList() throws IOException {
+    return Arrays
+        .stream(stdin.nextLine().split(" "))
+        .mapToInt(Integer::parseInt)
+        .toArray();
   }
 
-  @SuppressWarnings("unused")
-  private static Tuple nextTuple() throws IOException {
-    return new Tuple(nextArray());
+  static record Tuple(int left, int right) {}
+
+  static Tuple nextTuple() throws IOException {
+    return new Tuple(nextInt(), lastInt());
   }
 }
